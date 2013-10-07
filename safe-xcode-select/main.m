f6175861027eef1370a54a289e2b56f523728238
Jeff Kelley <SlaunchaMan@gmail.com>
2013-10-07 16:38:21 -0400

    Initial commit.

--- safe-xcode-select/main.m
+++ safe-xcode-select/main.m
@@ -6,17 +6,59 @@
 //  Copyright (c) 2013 Detroit Labs. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+
+
+static void usage(void);
+
 
 int main(int argc, const char * argv[])
 {
+    int returnValue = EXIT_SUCCESS;
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        // The first (and only) argument should be a path to an Xcode.app bundle
+        if (argc != 2) {
+            usage();
+            returnValue = EXIT_FAILURE;
+        }
+        else {
+            NSString *path =
+            [[NSString alloc] initWithCString:argv[1]
+                                     encoding:NSUTF8StringEncoding];
+            
+            BOOL isDirectory;
+            NSFileManager *fileManager = [NSFileManager defaultManager];
+            
+            if (path &&
+                [path hasSuffix:@".app"] &&
+                [fileManager fileExistsAtPath:path isDirectory:&isDirectory] &&
+                isDirectory) {
+                NSString *finalPath =
+                [[path stringByAppendingPathComponent:@"Contents"]
+                 stringByAppendingPathComponent:@"Developer"];
+                
+                NSTask *task =
+                [NSTask launchedTaskWithLaunchPath:@"/usr/bin/xcode-select"
+                                         arguments:@[ @"--switch", finalPath ]];
+                
+                [task waitUntilExit];
+                
+                returnValue = [task terminationStatus];
+            }
+            else {
+                usage();
+                returnValue = EXIT_FAILURE;
+            }
+        }
     }
-    return 0;
+    
+    return returnValue;
 }
 
+static void usage(void)
+{
+    printf("Usage: safe-xcode-select <Xcode path>\n");
+}
