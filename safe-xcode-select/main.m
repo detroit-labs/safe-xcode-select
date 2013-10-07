e3a5131a003c729bc70c886b2afae8990a3c93c4
Jeff Kelley <SlaunchaMan@gmail.com>
2013-10-07 16:22:06 -0400

    Initial Commit

---
+++ safe-xcode-select/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  safe-xcode-select
+//
+//  Created by Jeff Kelley on 10/7/13.
+//  Copyright (c) 2013 Detroit Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
